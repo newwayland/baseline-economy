f5eea00932d6fae471d9c9a3958ea5efa42480b8
Neil Wilson <neil@aldur.co.uk>
2020-06-14 19:26:41 +0100

    Update batch runner to add plots
    Batch runner now creates the Figure 4 plots

--- batch_run.py
+++ batch_run.py
@@ -1,12 +1,51 @@
 from BaselineEconomy.model import BaselineEconomyModel
 from mesa.batchrunner import BatchRunner
 from mesa.datacollection import DataCollector
+import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def excess_demand_figure(df):
+    plt.figure()
+    df.plot.hist(
+        bins=500,
+        y="Unsatisfied Demand",
+        density=True,
+        histtype='step',
+        align='left',
+        legend=False,
+        xlim=(0, 0.22),
+        xticks=[0, 0.05, 0.1, 0.15, 0.2]
+    )
+    plt.title("Excess Demand")
+    plt.xlabel("Unsatisfied demand (in %)")
+    plt.ylabel("Probability Density Function")
+    plt.savefig("/tmp/excess_demand.png")
+    plt.close()
+
+
+def employment_figure(df):
+    plt.figure()
+    df.plot.line(
+        x="Year",
+        y="Employed",
+        legend=False,
+        xticks=[0, 10, 20, 30, 40, 50],
+        xlim=(0, 50),
+        ylim=(950, 1000)
+    )
+    plt.title("Employment")
+    plt.xlabel("Years")
+    plt.ylabel("Employed households")
+    plt.savefig("/tmp/employment.png")
+    plt.close()
+
+
 br_params = {
+    "seed": [42],
     "num_households": [1000],
     "num_firms": [100],
-    "household_liquidity": [100, 1000, 10000],
+    "household_liquidity": [5000],
     "firm_liquidity": [0],
 }
 
@@ -37,4 +76,8 @@ if __name__ == "__main__":
                 .drop(list(range(burn_in)))
             )
             br_step_data = br_step_data.append(i_run_data, ignore_index=True)
-    br_step_data.to_csv("BaselineEconomyModel_Step_Data.csv")
+    br_step_data.to_csv("/tmp/BaselineEconomyModel_Step_Data.csv")
+    br_step_data["Year"] = (br_step_data.index.to_series() / 12)
+    plt.close('all')
+    excess_demand_figure(br_step_data)
+    employment_figure(br_step_data)
